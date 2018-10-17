5e4544495783182973f4931e349ea6b5ec91a6bb
ckim0531 <ckim0531@naver.com>
2018-10-18 01:04:10 +0900

    최신 앨범 가수 이름 2번 나오는 것 한 번만 나오게 처리 - 김수정

--- Lemon/WebContent/assets/js/main/main_new_album.js
+++ Lemon/WebContent/assets/js/main/main_new_album.js
@@ -1,18 +1,22 @@
-
-$(document).ready(function(){
-   $('.btn_new_album').each(function(){
-      $(this).find('li').each(function(){
-         $(this).hover(function() {
-            $(this).find(".overlay").css('display','block');
-            $(this).find('.singer').css('display','none');
-         }, function() { // 차트에 마우스 오버가 아닌 경우
-            $(this).find(".overlay").css('display','none');
-            $(this).find('.singer').css('display','block');
-            });
-      });
-   });
+// 마우스 오버 여부
+$(document).ready(function() {
+	$('.btn_new_album').each(function() {
+		$(this).find('li').each(function() {
+			// 마우스 오버시
+			$(this).hover(function() {
+	            $(this).find('.checkEllipsis').css('display','none');
+				$(this).find('.overlay').css('display', 'block');
+				$(this).find('.singer').css('display', 'none');
+			}, function() { // 마우스 오버가 아닌 경우
+	            $(this).find('.checkEllipsis').css('display','none');
+				$(this).find('.overlay').css('display', 'none');
+				$(this).find('.singer').css('display', 'block');
+			});
+		});
+	});
 });
 
+// 페이지 버튼
 var slideIndex = 1;
 
 showNewAlbum(slideIndex);
@@ -24,18 +28,18 @@ function plusNewAlbum(n) {
 function showNewAlbum(n) {
 	var i;
 	var x = document.getElementsByClassName("btn_new_album");
-	
+
 	if (n > x.length) {
 		slideIndex = 1
 	}
-	
+
 	if (n < 1) {
 		slideIndex = x.length
 	}
-	
+
 	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";  
+		x[i].style.display = "none";
 	}
-	
-	x[slideIndex-1].style.display = "block";  
+
+	x[slideIndex - 1].style.display = "block";
 }
