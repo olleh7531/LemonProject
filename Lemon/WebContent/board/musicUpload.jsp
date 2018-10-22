f0ebb8d7ee910ba8681951a142043171bfa121c3
w <w@DESKTOP>
2018-10-23 00:31:51 +0900

    최우성

--- Lemon/WebContent/board/musicUpload.jsp
+++ Lemon/WebContent/board/musicUpload.jsp
@@ -258,19 +258,14 @@
 
 <link rel="stylesheet" type="text/css" href="./assets/css/board/musicUpload.css" />
 
-<link rel="stylesheet" type="text/css" href="./assets/css/common/common.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/common/common_footer.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/common/common_main_menu_bxslider.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/common/common_font.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/font/nanumbarungothic.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/font/nanumgothic.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/menu/menu.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/menu/menu_common.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/menu/menu_search.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/menu/menu_search_realtime.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/menu/menu_banner.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/main/main_common.css">
-   <link rel="stylesheet" type="text/css" href="./assets/css/main/main_btn_page.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/common/common.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/common/menu.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/common/main_menu_bxslider.css"> <!-- 메인 / 메뉴 슬라이더 -->
+	<link rel="stylesheet" type="text/css" href="./assets/css/common/font.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/font/nanumbarungothic.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/font/nanumgothic.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/main/main.css">
+	<link rel="stylesheet" type="text/css" href="./assets/css/common/footer.css">
    
    <script type="text/javascript" src="./assets/bxslider-4-4.2.12/src/js/jquery.bxslider.js"></script>
    <script type="text/javascript" src="./assets/js/menu/menu_banner.js"></script>
--- Lemon/src/com/lemon/admin/music/action/MusicUploadAction.java
+++ Lemon/src/com/lemon/admin/music/action/MusicUploadAction.java
@@ -1,10 +1,17 @@
 package com.lemon.admin.music.action;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.mp3.MP3File;
+import org.jaudiotagger.tag.FieldKey;
+import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
 
 import com.lemon.admin.music.db.AMusicBean;
 import com.lemon.admin.music.db.AMusicDAO;
@@ -31,10 +38,37 @@ public class MusicUploadAction implements Action {
 		AMusicBean amb = new AMusicBean();
 		AMusicDAO amdao = new AMusicDAO();
 		while (files.hasMoreElements()) {
+			String musicfile = (String)files.nextElement();
 
-			String musicfile = files.nextElement();
-			musicfile = multi.getFilesystemName(musicfile);
-			String musicinfor = infor.nextElement();
+			System.out.println(musicfile);
+			File f = multi.getFile(musicfile);
+			MP3File mp3 = (MP3File) AudioFileIO.read(f);
+
+			AbstractID3v2Tag tag2 = mp3.getID3v2Tag();
+
+			Tag tag = mp3.getTag();
+			String title = tag.getFirst(FieldKey.TITLE);
+			String artist = tag.getFirst(FieldKey.ARTIST);
+			String album = tag.getFirst(FieldKey.ALBUM);
+			String year = tag.getFirst(FieldKey.YEAR);
+			String genre = tag.getFirst(FieldKey.GENRE);
+			String Lyrics = tag.getFirst(FieldKey.LYRICS);
+			String a18 = tag.getFirst(FieldKey.COVER_ART);
+		
+
+			// System.out.println("Tag : " + tag2);
+			System.out.println("Song Name : " + title);
+			System.out.println("Artist : " + artist);
+			System.out.println("Album : " + album);
+			System.out.println("Year : " + year);
+			System.out.println("Genre : " + genre);
+			System.out.println("Lyrics : " + Lyrics);
+			System.out.println("a18"+a18);
+	
+
+			
+
+/*			String musicinfor = infor.nextElement();
 			musicinfor = multi.getParameter(musicinfor);
 			amb.setMusicfile(musicfile);
 			String[] musicarr=musicinfor.split(",");
@@ -42,9 +76,9 @@ public class MusicUploadAction implements Action {
 			amb.setTrack_num(Integer.parseInt(musicarr[1]));
 			amb.setMusic_name((musicarr[2]));
 			amb.setSinger_name((musicarr[3]));
-			amb.setLyrics((musicarr[4]));
+			amb.setLyrics((musicarr[4]));*/
 
-			amdao.insertMusic(amb);
+//			amdao.insertMusic(amb);
 		}
 
 		response.setContentType("text/html; charset=UTF-8");
