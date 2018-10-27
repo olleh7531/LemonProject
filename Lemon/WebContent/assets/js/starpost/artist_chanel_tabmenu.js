980e98407d560ecf590c22291d5fafcc8212e12c
ckim0531 <ckim0531@naver.com>
2018-10-27 18:03:05 +0900

    아티스트 채널 탭 on 색상 완료 - 김수정

--- Lemon/WebContent/assets/js/starpost/artist_chanel_tabmenu.js
+++ Lemon/WebContent/assets/js/starpost/artist_chanel_tabmenu.js
@@ -3,11 +3,13 @@ $(function() {
 		var activeTab = $(this).attr('data-tab');
 		
 		$('.artist_chanel_tab li').removeClass('current');
-		
 		$('.tab_content').removeClass('current');
 		
 		$(this).addClass('current');
-		
 		$('#' + activeTab).addClass('current');
+		
+		// 색상 변경
+		$('.artist_chanel_tab li').removeClass("on");
+		$(this).addClass("on");
 	})
 });
